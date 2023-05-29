de005eca2e588c74ae0603c58a2d21a3e6c3f49f
Jon Erik Solheim <jones@restdb.io>
2023-05-29 14:56:14 +0200

    Updates

---
+++ precache-manifest.a7d5cdbb07eb480f146be1a41b88b094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fb20f6865abfc64c7b66f079c491c3",
+    "url": "/codehooks-io-react-todo/index.html"
+  },
+  {
+    "revision": "e9f183fd6738fc7b27f3",
+    "url": "/codehooks-io-react-todo/static/css/main.488e73bd.chunk.css"
+  },
+  {
+    "revision": "f4475b4646234bfc24e4",
+    "url": "/codehooks-io-react-todo/static/js/2.b96ae07b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/codehooks-io-react-todo/static/js/2.b96ae07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f183fd6738fc7b27f3",
+    "url": "/codehooks-io-react-todo/static/js/main.e3826e9f.chunk.js"
+  },
+  {
+    "revision": "370998ab1de616b5e0a6",
+    "url": "/codehooks-io-react-todo/static/js/runtime-main.08818396.js"
+  }
+]);
